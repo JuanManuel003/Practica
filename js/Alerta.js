1698ac0498ec860ef6e3cde639d270f95e9609ca
juan manuel <juanmanuelgoar@gmail.com>
2024-08-10 23:16:35 -0500

    inicio

---
+++ js/Alerta.js
@@ -0,0 +1,11 @@
+//Para crear cualquier variable de cualquier tipo 
+var variable = "juan";
+//var ingreso_texto = prompt("quien eres");//para mostrar un aventana de ingreso de texto
+
+function hola(nombre){
+    console.log("hola "+nombre+" dentro de una funcion")
+}
+
+if(true){
+    hola(variable);
+}
